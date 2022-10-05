e091cbca90cd3f36d87d32a37dd7074211b3c96f
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-05 16:01:25 -0700

    built out editpost.js

--- src/Components/EditPost/EditPost.js
+++ src/Components/EditPost/EditPost.js
@@ -1,5 +1,52 @@
-import { useHistory, useParams } from "react-router-dom";
-import { usePost } from "../../hooks/usePost";
-import { updatePost } from "../../services/posts";
+import { useHistory, useParams } from 'react-router-dom';
+import { updatePost } from '../../services/posts';
+import { usePost } from '../../hooks/usePost';
+
+export default function EditPost() {
+  const { id } = useParams();
+  const { postDetail, setPostDetail, loading, error, setError } = usePost(id);
+  const history = useHistory();
+
+  const handleSubmit = async () => {
+    try {
+      await updatePost(id, postDetail.title, postDetail.description);
+      history.push('/post');
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      <label>Title</label>
+      <input
+        type="text"
+        value={postDetail.title}
+        onChange={(e) => setPostDetail({ ...postDetail, title: e.target.value })}
+      />
+      <label>Description</label>
+      <input
+        type="text"
+        value={postDetail.description}
+        onChange={(e) => setPostDetail({ ...postDetail, description: e.target.value })}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+  
+
+
+  
+
 
 
