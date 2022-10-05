be25367edd3583b9ded782000002d0df1ada1a7d
Nathan Burgess <nathans.email.address@gmail.com>
2022-10-05 14:59:58 -0700

    basic routing set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Auth from './Components/Auth/Auth';
+import NewPost from './Components/NewPost/NewPost';
+import EditPost from './Components/EditPost/EditPost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Switch>
+        <Route path='/post/edit/:id' component={EditPost} />
+        <Route path="/auth/:type" component={Auth} />
+        <Route path='/post/new' component={NewPost} />
+        <Route path='/' component={Main} />
+        <Route path='*'>
+          <Redirect to='/auth/sign-in' />
+        </Route>
+      </Switch>
     </div>
   );
 }
