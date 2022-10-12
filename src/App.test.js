052f7373328cd8cef96ed0e365fc30f58d3b44d6
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-12 11:46:09 -0700

    wrote a passing test for auth page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+import * as authFns from './services/auth';
+
+
+jest.mock('./services/auth');
+
+
+const mockUser = {
+  id: 1,
+  aud: 'authenticated',
+  role: 'authenticated',
+  email: 'tanner@example.com',
+
+};
+
+test ('user can sign in', async () => {
+  authFns.getUser.mockReturnValue(null);
+  authFns.authUser.mockReturnValue(mockUser);
+
+  render(
+    <UserProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
+
+  const HeaderElement = screen.getByText(/Welcome to Bassbook 🎸/i);
+  expect(HeaderElement).toBeInTheDocument();
+
+  const signInLink = screen.getByText('Sign In');
+  fireEvent.click(signInLink);
+
+  const emailInput = screen.getByLabelText('Email');
+  fireEvent.change(emailInput, { target: { value: 'text' } });
+  expect(emailInput.value).toBe('text');
+  
+  const passwordInput = screen.getByLabelText('Password', { type: 'password' });
+  fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+  const signInButton = screen.getByText('Enter');
+  fireEvent.click(signInButton);
+
 });
+
+
+
+
+
+// write test for auth
+
+
+
+
+
+
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -22,10 +22,12 @@ export default function Auth() {
   return (
     <div className='auth-form-container'>
       <div className={`auth-form ${isFish ? 'fish' : 'music'}`}>
-        <label>Email</label>
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <label>Password</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label htmlFor='Email'>Email
+          <input aria-label='Email' name='Email' type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>Password
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
         <button onClick={async () => {
           await authUser(email, password, type);
           getNewUser();
