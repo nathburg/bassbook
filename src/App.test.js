e3d1b17265414e18edd6bedc8fa46cf6cac156cd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-10-12 13:29:48 -0500

    Set up testing and test if header renders to screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { UserProvider } from './context/UserContext';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('renders Welcome to Bassbook header', () => {
+  render(
+    <MemoryRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </MemoryRouter>
+  );
+  const linkElement = screen.getByText(/welcome to bassbook/i);
+  expect(linkElement).toBeInTheDocument();
 });
